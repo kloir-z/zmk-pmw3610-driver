b7484c2b72fec065ca53130280a407c919a05a1e
kloir-z <kosuke.otsu@gmail.com>
2025-02-20 23:54:51 +0900

    スクロール感度調整

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -741,16 +741,22 @@ static int pmw3610_report_data(const struct device *dev) {
             data->scroll_delta_y += accel_y;
                     
             if (abs(data->scroll_delta_y) > CONFIG_PMW3610_SCROLL_TICK) {
-                input_report_rel(dev, INPUT_REL_WHEEL,
-                            data->scroll_delta_y > 0 ? PMW3610_SCROLL_Y_NEGATIVE : PMW3610_SCROLL_Y_POSITIVE,
-                            true, K_FOREVER);
+                int event_count = abs(data->scroll_delta_y) / CONFIG_PMW3610_SCROLL_TICK;
+                for (int i = 0; i < event_count; i++) {
+                    input_report_rel(dev, INPUT_REL_WHEEL,
+                                data->scroll_delta_y > 0 ? PMW3610_SCROLL_Y_NEGATIVE : PMW3610_SCROLL_Y_POSITIVE,
+                                true, K_FOREVER);
+                }
+                data->scroll_delta_y = data->scroll_delta_y % CONFIG_PMW3610_SCROLL_TICK;
                 data->scroll_delta_x = 0;
-                data->scroll_delta_y = 0;
             } else if (abs(data->scroll_delta_x) > CONFIG_PMW3610_SCROLL_TICK) {
-                input_report_rel(dev, INPUT_REL_HWHEEL,
-                            data->scroll_delta_x > 0 ? PMW3610_SCROLL_X_NEGATIVE : PMW3610_SCROLL_X_POSITIVE,
-                            true, K_FOREVER);
-                data->scroll_delta_x = 0;
+                int event_count = abs(data->scroll_delta_x) / CONFIG_PMW3610_SCROLL_TICK;
+                for (int i = 0; i < event_count; i++) {
+                    input_report_rel(dev, INPUT_REL_HWHEEL,
+                                data->scroll_delta_x > 0 ? PMW3610_SCROLL_X_NEGATIVE : PMW3610_SCROLL_X_POSITIVE,
+                                true, K_FOREVER);
+                }
+                data->scroll_delta_x = data->scroll_delta_x % CONFIG_PMW3610_SCROLL_TICK;
                 data->scroll_delta_y = 0;
             }
         }
